042b82972492edbc5237b2f647a893faf4107b5e
Janardhana R <janardhanar@Janardhanas-MacBook-Air.local>
2026-09-16 06:34:00 +0530

    switched to astro and added basic site template

---
+++ src/consts.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Janardhana R';
+export const SITE_DESCRIPTION =
+	'Janardhana R — Lead Engineer focused on Developer Experience, Platform Engineering, Build Systems, and developer tooling.';
